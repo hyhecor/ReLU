6bf01f4853de0fa87258e97690490e3034357428
hyhecor <hyhecor@gmail.com>
2020-10-30 17:01:41 +0900

    FP

--- softmax.go
+++ softmax.go
@@ -3,26 +3,37 @@ package activations
 import "math"
 
 //SoftMax is exp(x) / sum(exp(x))
-func SoftMax(x ...float64) (out []float64) {
-	out = make([]float64, len(x))
+func SoftMax(x ...float64) []float64 {
+	out := make([]float64, len(x))
 
-	inputSum := SumWithBlackBox(func(f float64) float64 {
-		return math.Exp(f)
-	}, x...)
+	sum := func(a, b float64) float64 {
+		return a + b
+	}
 
 	for i, f := range x {
-		s := math.Exp(f) / inputSum
+		s := math.Exp(f) / Fold(sum, Map(math.Exp, x))
 		out[i] = s
+	}
+
+	return out
+}
+
+//Map (higher-order function)
+func Map(fn func(x float64) float64, x []float64) []float64 {
+	out := make([]float64, len(x))
+	for i, v := range x {
+		out[i] = fn(v)
 	}
 	return out
 }
 
-// SumWithBlackBox sum
-func SumWithBlackBox(blackbox func(x float64) float64, x ...float64) (out float64) {
+//Fold (higher-order function)
+func Fold(fn func(a, b float64) float64, x []float64) float64 {
+	out := float64(0)
 	for _, v := range x {
-		out += blackbox(v)
+		out = fn(out, v)
 	}
-	return
+	return out
 }
 
 func init() {
