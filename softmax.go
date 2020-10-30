d6dab11b41dbc9f6328cc799bc418ab2041fc1bf
hyhecor <hyhecor@gmail.com>
2020-10-30 17:29:18 +0900

    calc := func(a []float64) func(f float64) float64 { 		return func(e float64) float64 { 			return math.Exp(e) / Fold(float64(0), sum, Map(math.Exp, a)) 		} 	}

--- softmax.go
+++ softmax.go
@@ -3,19 +3,20 @@ package activations
 import "math"
 
 //SoftMax is exp(x) / sum(exp(x))
-func SoftMax(x ...float64) []float64 {
-	out := make([]float64, len(x))
+func SoftMax(x ...float64) (out []float64) {
+	out = make([]float64, len(x))
 
 	sum := func(a, b float64) float64 {
 		return a + b
 	}
 
-	for i, f := range x {
-		s := math.Exp(f) / Fold(sum, Map(math.Exp, x))
-		out[i] = s
+	calc := func(a []float64) func(f float64) float64 {
+		return func(e float64) float64 {
+			return math.Exp(e) / Fold(float64(0), sum, Map(math.Exp, a))
+		}
 	}
 
-	return out
+	return Map(calc(x), x)
 }
 
 //Map (higher-order function)
@@ -28,8 +29,8 @@ func Map(fn func(x float64) float64, x []float64) []float64 {
 }
 
 //Fold (higher-order function)
-func Fold(fn func(a, b float64) float64, x []float64) float64 {
-	out := float64(0)
+func Fold(def float64, fn func(a, b float64) float64, x []float64) float64 {
+	out := def
 	for _, v := range x {
 		out = fn(out, v)
 	}
